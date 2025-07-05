2cda471bac2f5a5bb7e417720d5ff82b37242e84
OtakuSanskar <sanskarsahu765@gmail.com>
2025-07-05 18:04:00 +0530

    FlatList Created

--- App.js
+++ App.js
@@ -3,24 +3,34 @@ import {
   View,
   Text,
   StyleSheet,
-  SafeAreaView
+  FlatList
 } from "react-native";
+import { ArrayColors } from "./src/components/RawData";
+import ColorBox from "./src/components/ColorBox";
+
 const App = () => {
-  return (
-      <View>
-        <Text style={style.top}>Hello Sanskar!!</Text>
-      </View>
-  );
+  return
+  <FlatList
+    style={style.container}
+    data="ArrayColors"
+    keyExtractor={(item) => item.hex}
+    renderItem={({ item }) => (
+      <ColorBox hexValue={item.hex} name={item.name} />)}
+    ListheaderComponent={
+    <Text style={{fontWeight: "800", color: "black", marginBottom: 20}}>
+      Color List
+    </Text>
+  }
+  />
 }
+
 
 export default App;
 
 const style = StyleSheet.create({
-  top: {
-    padding: 100,
-    backgroundColor: "blue",
-    color: "white",
-    fontSize: 30,
-    textAlign: "center",
+  container: {
+    paddingTop: 30,
+    paddingHorizontal: 20,
+    paddingBottom: 10,
   }
 })
--- scr/components/ColorBox.js
+++ scr/components/ColorBox.js
@@ -1,1 +1,33 @@
+import React from "react";
+import {View, Text, StyleSheet, SafeAreaView, FlatList} from "react-native";
 
+const ColorBox = props => {
+  const backgroundStyle = {
+    backgroundColor: props.hexValue,
+  }
+  return(
+    <View style={[styles.box, backgroundStyle]}>
+      <Text style={styles.text}>
+        {props.name} {props.hexValue}
+      </Text>
+    </View>
+  )
+}
+
+export default ColorBox;
+
+const styles = StyleSheet.create({
+  box: {
+    padding: 10,
+    borderRadius: 3,
+    justifyContent: "center",
+    alignItems: "center",
+    marbottom: 10,
+  },
+  text: {
+    fontweight: "800",
+    color: "white",
+    paddingTop: 25,
+    fontSize: 16,
+  }
+})
