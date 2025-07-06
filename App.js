bd38bb5015b400b79727a2054110fd70267b561a
OtakuSanskar <sanskarsahu765@gmail.com>
2025-07-07 01:14:12 +0530

    Add navigation setup and create Home screen component

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import React from "react";
+import {NavigationContainer} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./Screens/Home";
 import {
   View,
   Text,
   StyleSheet,
   SafeAreaView
 } from "react-native";
+
+const StackNavigator = createNativeStackNavigator();
 const App = () => {
   return (
-      <View>
-        <Text style={style.top}>Hello Sanskar!!</Text>
-      </View>
+      <NavigationContainer>
+        <StackNavigator.Navigator>
+           <StackNavigator.Screen name="Home" component={Home} />
+        </StackNavigator.Navigator>
+      </NavigationContainer>
   );
 }
 
